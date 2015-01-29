2a88b4a215abf8e3cce9fdd6b7a8f3010e5e93f3
Sergey Konoplev <sergey.konoplev@nitropdf.com>
2015-01-28 19:33:34 -0800

    Refactore the table archiving output, added stats.

--- bin/archive_tables.sh
+++ bin/archive_tables.sh
@@ -8,7 +8,7 @@
 # executes the ARCHIVE_COMMAND_BEFORE_DROP function, drops the table
 # and executes the ARCHIVE_COMMAND_AFTER_DROP function. If
 # ARCHIVE_LOCAL_DIR is not specified or is empty, then it archives
-# partitions directly to ARCHIVE_ARCHIVE_DIR. The values of database
+# tables directly to ARCHIVE_ARCHIVE_DIR. The values of database
 # name, table name and archived file name are sent to the
 # ARCHIVE_COMMAND_*_DROP functions as $1, $2 and $3 accordingly. To
 # turn dry run mode on set ARCHIVE_DRY_RUN to 'true'.
@@ -28,8 +28,10 @@ error=$(mkdir -p $ARCHIVE_ARCHIVE_DIR 2>&1) ||  \
     die "Can not make archive directory $ARCHIVE_ARCHIVE_DIR: $error."
 
 for dbname in $ARCHIVE_DBNAME_LIST; do
+    dump_start_time=$(timer)
+
     part_list=$($PSQL -XAt -F '.' -c "$ARCHIVE_PARTS_SQL" $dbname 2>&1) || \
-        die "Can not get a partition list: $part_list."
+        die "Can not get a table list: $part_list."
 
     if [ -z "$part_list" ]; then
         info "There is nothing to archive in the database $dbname."
@@ -57,32 +59,40 @@ for dbname in $ARCHIVE_DBNAME_LIST; do
         if ! $ARCHIVE_DRY_RUN; then
             error=$($PGDUMP -F c -Z 2 -t $part \
                         -f $ARCHIVE_LOCAL_DIR/$dbname/$file $dbname 2>&1) || \
-                die "Can not dump partition $part from $dbname: $error."
+                die "Can not dump table $part from $dbname: $error."
 
             error=$(ARCHIVE_COMMAND_BEFORE_DROP $dbname $part $file 2>&1) || \
                 die "Before-command error for $part from $dbname: $error."
 
             error=$($PSQL -c "DROP TABLE $part;" $dbname 2>&1) || \
-                die "Can not drop partition $part from $dbname: $error."
+                die "Can not drop table $part from $dbname: $error."
 
             error=$(ARCHIVE_COMMAND_AFTER_DROP $dbname $part $file 2>&1) || \
                 die "After-command error for $part from $dbname: $error."
 
-            info "Partition $part from $dbname dumped in $dbname/$file."
+            info "Table $part from $dbname dumped in $dbname/$file."
         else
-            info "Partition $part from $dbname can be dumped in $dbname/$file."
+            info "Table $part from $dbname can be dumped in $dbname/$file."
         fi
     done
 
+    dump_time=$(( ${dump_time:0} + $(timer $dump_start_time) ))
+
     if ! $ARCHIVE_DRY_RUN; then
         if [ $ARCHIVE_LOCAL_DIR != $ARCHIVE_ARCHIVE_DIR ]; then
+            sync_start_time=$(timer)
+
             error=$($RSYNC $ARCHIVE_LOCAL_DIR/$dbname \
                     $ARCHIVE_ARCHIVE_DIR 2>&1) || \
                 die "Can sync directory $dbname to archive: $error."
             error=$(rm -rf $ARCHIVE_LOCAL_DIR/$dbname 2>&1) || \
                 die "Can not clean directory $dbname localy: $error."
 
+            sync_time=$(( ${sync_time:-0} + $(timer $sync_start_time) ))
+
             info "Moved directory $dbname to archive."
         fi
     fi
 done
+
+info "Execution time, s: dump ${dump_time:-N/A}, sync ${sync_time:-N/A}."
