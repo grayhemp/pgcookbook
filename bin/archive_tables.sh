458bd6d2a45887fbce6651ec94810945f3fb0f70
Sergey Konoplev <sergey.konoplev@nitropdf.com>
2015-04-03 14:40:57 -0700

    Updated archive_tables.sh to support structured logging.

--- bin/archive_tables.sh
+++ bin/archive_tables.sh
@@ -20,59 +20,99 @@
 source $(dirname $0)/config.sh
 source $(dirname $0)/utils.sh
 
-if [ -z "$ARCHIVE_LOCAL_DIR" ]; then
+if [[ -z "$ARCHIVE_LOCAL_DIR" ]]; then
     ARCHIVE_LOCAL_DIR=$ARCHIVE_ARCHIVE_DIR
 fi
 
-error=$(mkdir -p $ARCHIVE_ARCHIVE_DIR 2>&1) ||  \
-    die "Can not make archive directory $ARCHIVE_ARCHIVE_DIR: $error."
+error=$(mkdir -p $ARCHIVE_ARCHIVE_DIR 2>&1) ||
+    die "$(declare -pA a=(
+        ['1/message']='Can not make an archive directory'
+        ['2/archive_dir']=$ARCHIVE_ARCHIVE_DIR
+        ['3m/detail']=$error))"
 
 for dbname in $ARCHIVE_DBNAME_LIST; do
     dump_start_time=$(timer)
 
-    part_list=$($PSQL -XAt -F '.' -c "$ARCHIVE_PARTS_SQL" $dbname 2>&1) || \
-        die "Can not get a table list: $part_list."
+    part_list=$($PSQL -XAt -F '.' -c "$ARCHIVE_PARTS_SQL" $dbname 2>&1) ||
+        die "$(declare -pA a=(
+            ['1/message']='Can not get a table list'
+            ['2m/detail']=$part_list))"
 
     if [ -z "$part_list" ]; then
-        info "There is nothing to archive in the database $dbname."
+        info "$(declare -pA a=(
+            ['1/message']='There is nothing to archive'
+            ['2/database']=$dbname))"
+
         continue
     fi
 
     ts=$(date +%Y%m%d%H%M)
 
     if ! $ARCHIVE_DRY_RUN; then
-        error=$(mkdir -p $ARCHIVE_LOCAL_DIR/$dbname 2>&1) || \
-            die "Can not make a database directory for $dbname: $error."
+        error=$(mkdir -p $ARCHIVE_LOCAL_DIR/$dbname 2>&1) ||
+            die "$(declare -pA a=(
+                ['1/message']='Can not make a database directory'
+                ['2/database']=$dbname
+                ['3m/detail']=$error))"
     fi
 
     for part in $part_list; do
         file=$part.$ts.dump
 
-        if [ $ARCHIVE_LOCAL_DIR != $ARCHIVE_ARCHIVE_DIR ]; then
-            [ -f $ARCHIVE_LOCAL_DIR/$dbname/$file ] && \
-                die "File $dbname/$file already exists locally."
+        if [[ $ARCHIVE_LOCAL_DIR != $ARCHIVE_ARCHIVE_DIR ]]; then
+            [[ -f $ARCHIVE_LOCAL_DIR/$dbname/$file ]] &&
+                die "$(declare -pA a=(
+                    ['1/message']='File already exists locally'
+                    ['2/database']=$dbname
+                    ['3/file']=$file))"
         fi
 
-        [ -f $ARCHIVE_ARCHIVE_DIR/$dbname/$file ] && \
-            die "File $dbname/$file allready exists in archive."
+        [[ -f $ARCHIVE_ARCHIVE_DIR/$dbname/$file ]] &&
+            die "$(declare -pA a=(
+                ['1/message']='File already exists in archive'
+                ['2/database']=$dbname
+                ['3/file']=$file))"
 
         if ! $ARCHIVE_DRY_RUN; then
-            error=$($PGDUMP -F c -Z 2 -t $part \
-                        -f $ARCHIVE_LOCAL_DIR/$dbname/$file $dbname 2>&1) || \
-                die "Can not dump table $part from $dbname: $error."
+            error=$(
+                $PGDUMP -F c -Z 2 -t $part -f $ARCHIVE_LOCAL_DIR/$dbname/$file \
+                    $dbname 2>&1) ||
+                die "$(declare -pA a=(
+                    ['1/message']='Can not dump the table'
+                    ['2/database']=$dbname
+                    ['3/table']=$part
+                    ['4m/detail']=$error))"
 
-            error=$(ARCHIVE_COMMAND_BEFORE_DROP $dbname $part $file 2>&1) || \
-                die "Before-command error for $part from $dbname: $error."
+            error=$(ARCHIVE_COMMAND_BEFORE_DROP $dbname $part $file 2>&1) ||
+                die "$(declare -pA a=(
+                    ['1/message']='Before-drop command error'
+                    ['2/database']=$dbname
+                    ['3/table']=$part
+                    ['4m/detail']=$error))"
 
-            error=$($PSQL -c "DROP TABLE $part;" $dbname 2>&1) || \
-                die "Can not drop table $part from $dbname: $error."
+            error=$($PSQL -c "DROP TABLE $part;" $dbname 2>&1) ||
+                die "$(declare -pA a=(
+                    ['1/message']='Can not drop the table'
+                    ['2/database']=$dbname
+                    ['3/table']=$part
+                    ['4m/detail']=$error))"
 
-            error=$(ARCHIVE_COMMAND_AFTER_DROP $dbname $part $file 2>&1) || \
-                die "After-command error for $part from $dbname: $error."
+            error=$(ARCHIVE_COMMAND_AFTER_DROP $dbname $part $file 2>&1) ||
+                die "$(declare -pA a=(
+                    ['1/message']='After-drop command error'
+                    ['2/database']=$dbname
+                    ['3/table']=$part
+                    ['4m/detail']=$error))"
 
-            info "Table $part from $dbname dumped in $dbname/$file."
+            info "$(declare -pA a=(
+                ['1/message']='Table has been dumped'
+                ['2/database']=$dbname
+                ['3/table']=$part))"
         else
-            info "Table $part from $dbname can be dumped in $dbname/$file."
+            info "$(declare -pA a=(
+                ['1/message']='Table can be dumped'
+                ['2/database']=$dbname
+                ['3/table']=$part))"
         fi
     done
 
@@ -82,17 +122,29 @@ for dbname in $ARCHIVE_DBNAME_LIST; do
         if [ $ARCHIVE_LOCAL_DIR != $ARCHIVE_ARCHIVE_DIR ]; then
             sync_start_time=$(timer)
 
-            error=$($RSYNC $ARCHIVE_LOCAL_DIR/$dbname \
-                    $ARCHIVE_ARCHIVE_DIR 2>&1) || \
-                die "Can sync directory $dbname to archive: $error."
-            error=$(rm -rf $ARCHIVE_LOCAL_DIR/$dbname 2>&1) || \
-                die "Can not clean directory $dbname localy: $error."
+            error=$(
+                $RSYNC $ARCHIVE_LOCAL_DIR/$dbname $ARCHIVE_ARCHIVE_DIR 2>&1) ||
+                die "$(declare -pA a=(
+                    ['1/message']='Can not sync the directory to archive'
+                    ['2/database']=$dbname
+                    ['3m/detail']=$error))"
+
+            error=$(rm -rf $ARCHIVE_LOCAL_DIR/$dbname 2>&1) ||
+                die "$(declare -pA a=(
+                    ['1/message']='Can not remove the directory locally'
+                    ['2/database']=$dbname
+                    ['3m/detail']=$error))"
 
             sync_time=$(( ${sync_time:-0} + $(timer $sync_start_time) ))
 
-            info "Moved directory $dbname to archive."
+            info "$(declare -pA a=(
+                ['1/message']='Directory has been moved to archive'
+                ['2/database']=$dbname))"
         fi
     fi
 done
 
-info "Execution time, s: dump ${dump_time:-N/A}, sync ${sync_time:-N/A}."
+info "$(declare -pA a=(
+    ['1/message']='Execution time, s'
+    ['2/dump']=${dump_time:-null}
+    ['3/sync']=${sync_time:-null}))"
