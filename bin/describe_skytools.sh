da764ed9059ff50a5b4f8ae6117c0fe2ba9f6289
Sergey Konoplev <sergey.konoplev@nitropdf.com>
2015-04-09 16:32:53 -0700

    Updated describe_skytools.sh to support structured logging and moved a non descriptional stuff to stat_skytools.sh.

--- bin/describe_skytools.sh
+++ bin/describe_skytools.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# describe_skytools.sh - Skytools description script.
+# describe_skytools.sh - provides details about a Skytools setup.
 #
-# Collects info about Skytools. Compatible with versions >=3.0.
+# Collects and prints out:
+#
+# - per database PgQ version
 #
 # Copyright (c) 2015 Sergey Konoplev
 #
@@ -11,84 +13,42 @@
 source $(dirname $0)/config.sh
 source $(dirname $0)/utils.sh
 
-# per database version
-# per database top queues by ticker lag
-# per database top consumers by lag
-# per database top consumers by last seen age
+# per database  PgQ version
 
 version_sql=$(cat <<EOF
 SELECT pgq.version()
 EOF
 )
 
-queue_lag_sql=$(cat <<EOF
-SELECT queue_name, extract(epoch from ticker_lag)::integer
-FROM pgq.get_queue_info()
-ORDER BY 2 DESC LIMIT 5
-EOF
-)
-
-consumer_lag_sql=$(cat <<EOF
-SELECT queue_name, consumer_name, extract(epoch from lag)::integer
-FROM pgq.get_consumer_info()
-ORDER BY 3 DESC LIMIT 5
-EOF
-)
-
-consumer_last_seen_sql=$(cat <<EOF
-SELECT queue_name, consumer_name, extract(epoch from last_seen)::integer
-FROM pgq.get_consumer_info()
-ORDER BY 3 DESC LIMIT 5
-EOF
-)
-
 (
     db_list=$(
-        $PSQL -XAt -c "SELECT datname FROM pg_database WHERE datallowconn"
-        2>&1) ||
-        die "Can not get a database list: $src."
+        $PSQL -XAt -c \
+            "SELECT datname FROM pg_database WHERE datallowconn" 2>&1) ||
+        die "$(declare -pA a=(
+            ['1/message']='Can not get a database list'
+            ['2m/detail']=$db_list))"
 
     (
         for db in $db_list; do
             schema_line=$($PSQL -XAtc '\dn pgq' $db 2>&1) ||
-                die "Can not check pgq schema for $db: $schema_line."
+                die "$(declare -pA a=(
+                    ['1/message']='Can not check the pgq schema'
+                    ['2/database']=$db
+                    ['3m/detail']=$schema_line))"
 
             [ -z "$schema_line" ] && continue
 
             (
-                result=$(
-                    $PSQL -XAt -c "$version_sql" $db 2>&1) ||
-                    die "Can not get a version data for $db: $result."
+                result=$($PSQL -XAt -c "$version_sql" $db 2>&1) ||
+                    die "$(declare -pA a=(
+                        ['1/message']='Can not get a version data'
+                        ['2/database']=$db
+                        ['3m/detail']=$result))"
 
-                info "PgQ version for $db: ${result:-N/A}."
-            )
-
-            (
-                result=$(
-                    $PSQL -XAt -R ', ' -F ' '  -c "$queue_lag_sql" $db 2>&1) ||
-                    die "Can not get a queue lag data for $db: $result."
-
-                info "Top queues by ticker lag for $db, s: ${result:-N/A}."
-            )
-
-            (
-                result=$(
-                    $PSQL -XAt -R ', ' -F ' '  -c "$consumer_lag_sql" $db \
-                    2>&1) ||
-                    die "Can not get a consumer lag data for $db: $result."
-
-                info "Top consumers by lag for $db, s: ${result:-N/A}."
-            )
-
-            (
-                result=$(
-                    $PSQL -XAt -R ', ' -F ' '  -c "$consumer_last_seen_sql" \
-                    $db 2>&1) ||
-                    die "Can not get a consumers last seen data for $db:" \
-                        "$result."
-
-                info "Top consumers by last seen age for $db, s:" \
-                     "${result:-N/A}."
+                info "$(declare -pA a=(
+                    ['1/message']='PgQ version'
+                    ['2/database']=$db
+                    ['3/version']=${result:-null}))"
             )
         done
     )
